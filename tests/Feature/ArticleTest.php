d7ed07c18f2b0c1590734507fc8f0eab2a7a37a0
HuntingRathalos <huntingandrathalos@gmail.com>
2021-11-28 00:12:00 +0900

    いいねされているかを判定するメソッドのテストの続き

--- tests/Feature/ArticleTest.php
+++ tests/Feature/ArticleTest.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Article;
+use App\User;
 
 
 class ArticleTest extends TestCase
@@ -17,6 +18,29 @@ class ArticleTest extends TestCase
         $article = factory(Article::class)->create();
 
         $result = $article->isLikedBy(null);
+
+        $this->assertFalse($result);
+    }
+
+    public function testIsLikedByTheUser()
+    {
+        $article = factory(Article::class)->create();
+        $user = factory(User::class)->create();
+        $article->likes()->attach($user);
+
+        $result = $article->isLikedBy($user);
+
+        $this->assertTrue($result);
+    }
+
+    public function testIsLikedByAnother()
+    {
+        $article = factory(Article::class)->create();
+        $user = factory(User::class)->create();
+        $another = factory(User::class)->create();
+        $article->likes()->attach($another);
+
+        $result = $article->isLikedBy($user);
 
         $this->assertFalse($result);
     }
